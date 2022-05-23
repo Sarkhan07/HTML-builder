ed455ab095c93e268fbfca40efbefc53b157edb6
Sarkhan <abdullayevsarxan92@gmail.com>
2022-05-23 23:44:28 +0400

    feat: forth task is completed

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir() {
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+        if (err) throw err;
+        console.log('New folder is created');
+    });
+
+    fs.readdir(path.join(__dirname, 'files'), (err, data) => {
+        data.forEach((file) => {
+            fs.copyFile(
+                path.join(__dirname, 'files', file),
+                path.join(__dirname, 'files-copy', file),
+                (err) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        console.log(
+                            'All files are copied to new folder "files-copy"'
+                        );
+                    }
+                }
+            );
+        });
+    });
+}
+
+copyDir();
