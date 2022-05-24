a364d19d8abe0d3c6124ba09ed407b40787eed55
Sarkhan <abdullayevsarxan92@gmail.com>
2022-05-25 02:22:54 +0400

    adding last task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,12 +3,37 @@ const fsPromises = fs.promises;
 const path = require('path');
 
 function copyDir() {
+    fs.writeFile(
+        path.join(__dirname, 'project-dist', 'index.html'),
+        '',
+        function (err) {
+            if (err) throw err;
+        }
+    );
+
+    fs.writeFile(
+        path.join(__dirname, 'project-dist', 'style.css'),
+        '',
+        (err) => {
+            if (err) throw err;
+        }
+    );
+    fs.readdir(path.join(__dirname, 'styles'), (err, data) => {
+        data.forEach((style) => {
+            fs.readFile(path.join(__dirname, 'styles', style), (err, data) => {
+                fs.appendFile(
+                    path.join(__dirname, 'project-dist', 'style.css'),
+                    data
+                );
+            });
+        });
+    });
+
     fs.mkdir(
         path.join(__dirname, 'project-dist'),
         { recursive: false },
         (err) => {
             if (err) throw err;
-            console.log('New folder is created');
         }
     );
 
@@ -70,6 +95,7 @@ function copyAssets() {
     });
 
     fs.readdir(path.join(__dirname, 'assets', 'img'), (err, data) => {
+        copyDir();
         data.forEach((file) => {
             fs.copyFile(
                 path.join(__dirname, 'assets', 'img', file),
@@ -88,6 +114,7 @@ function copyAssets() {
     });
 
     fs.readdir(path.join(__dirname, 'assets', 'svg'), (err, data) => {
+        copyDir();
         data.forEach((file) => {
             fs.copyFile(
                 path.join(__dirname, 'assets', 'svg', file),
@@ -107,27 +134,3 @@ function copyAssets() {
 }
 
 copyAssets();
-
-function copyStyle() {
-    fs.readdir(path.join(__dirname, 'styles'), (err, data) => {
-        data.forEach((style) => {
-            if (path.extname(style) === '.css') {
-                fs.readFile(
-                    path.join(__dirname, 'styles', style),
-                    (err, data) => {
-                        if (err) return console.error(err);
-                        fs.appendFile(
-                            path.join(__dirname, 'project-dist', 'style.css'),
-                            data,
-                            (err) => {
-                                if (err) return console.error(err);
-                            }
-                        );
-                    }
-                );
-            }
-        });
-    });
-}
-
-copyStyle();
