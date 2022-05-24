b4fe8adf891fc0623e4a0cd5eadd19161ea7242f
Sarkhan <abdullayevsarxan92@gmail.com>
2022-05-25 00:20:44 +0400

    fix: fixing problem with adding assets

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,14 +1,28 @@
 const fs = require('fs');
+const fsPromises = fs.promises;
 const path = require('path');
 
-function copyFonts() {
+function copyDir() {
     fs.mkdir(
-        path.join(
-            __dirname,
-            'project-dist',
-            'assets',
-            'fonts' && 'img' && 'svg'
-        ),
+        path.join(__dirname, 'project-dist'),
+        { recursive: false },
+        (err) => {
+            if (err) throw err;
+            console.log('New folder is created');
+        }
+    );
+
+    fs.mkdir(
+        path.join(__dirname, 'project-dist', 'assets'),
+        { recursive: false },
+        (err) => {
+            if (err) throw err;
+            console.log('New folder is created');
+        }
+    );
+
+    fs.mkdir(
+        path.join(__dirname, 'project-dist', 'assets', 'fonts'),
         { recursive: true },
         (err) => {
             if (err) throw err;
@@ -16,59 +30,93 @@ function copyFonts() {
         }
     );
 
-    fs.readdir(path.join(__dirname, 'assets', 'fonts'), (err, data) => {
-        data.forEach((file) => {
-            fs.copyFile(
-                path.join(__dirname, 'assets', 'fonts', file),
-                path.join(__dirname, 'project-dist', 'assets', 'fonts', file),
-                (err) => {
-                    if (err) {
-                        throw err;
-                    } else {
-                        console.log(
-                            'All files are copied to new folder "files-copy"'
-                        );
-                    }
-                }
-            );
-        });
-    });
+    fs.mkdir(
+        path.join(__dirname, 'project-dist', 'assets', 'img'),
+        { recursive: true },
+        (err) => {
+            if (err) throw err;
+            console.log('New folder is created');
+        }
+    );
 
-    fs.readdir(path.join(__dirname, 'assets', 'img'), (err, data) => {
-        data.forEach((file) => {
-            fs.copyFile(
-                path.join(__dirname, 'assets', 'img', file),
-                path.join(__dirname, 'project-dist', 'assets', 'img', file),
-                (err) => {
-                    if (err) {
-                        throw err;
-                    } else {
-                        console.log(
-                            'All files are copied to new folder "files-copy"'
-                        );
-                    }
-                }
-            );
-        });
-    });
-
-    fs.readdir(path.join(__dirname, 'assets', 'svg'), (err, data) => {
-        data.forEach((file) => {
-            fs.copyFile(
-                path.join(__dirname, 'assets', 'svg', file),
-                path.join(__dirname, 'project-dist', 'assets', 'svg', file),
-                (err) => {
-                    if (err) {
-                        throw err;
-                    } else {
-                        console.log(
-                            'All files are copied to new folder "files-copy"'
-                        );
-                    }
-                }
-            );
-        });
-    });
+    fs.mkdir(
+        path.join(__dirname, 'project-dist', 'assets', 'svg'),
+        { recursive: false },
+        (err) => {
+            if (err) throw err;
+            console.log('New folder is created');
+        }
+    );
 }
 
-copyFonts();
+fs.readdir(path.join(__dirname, 'assets', 'fonts'), (err, data) => {
+    copyDir();
+    data.forEach((file) => {
+        fs.copyFile(
+            path.join(__dirname, 'assets', 'fonts', file),
+            path.join(__dirname, 'project-dist', 'assets', 'fonts', file),
+            (err) => {
+                if (err) {
+                    throw err;
+                } else {
+                    console.log(
+                        'All files are copied to new folder "files-copy"'
+                    );
+                }
+            }
+        );
+    });
+});
+
+fs.readdir(path.join(__dirname, 'assets', 'img'), (err, data) => {
+    data.forEach((file) => {
+        fs.copyFile(
+            path.join(__dirname, 'assets', 'img', file),
+            path.join(__dirname, 'project-dist', 'assets', 'img', file),
+            (err) => {
+                if (err) {
+                    throw err;
+                } else {
+                    console.log(
+                        'All files are copied to new folder "files-copy"'
+                    );
+                }
+            }
+        );
+    });
+});
+
+fs.readdir(path.join(__dirname, 'assets', 'svg'), (err, data) => {
+    data.forEach((file) => {
+        fs.copyFile(
+            path.join(__dirname, 'assets', 'svg', file),
+            path.join(__dirname, 'project-dist', 'assets', 'svg', file),
+            (err) => {
+                if (err) {
+                    throw err;
+                } else {
+                    console.log(
+                        'All files are copied to new folder "files-copy"'
+                    );
+                }
+            }
+        );
+    });
+});
+
+// fs.readdir(path.join(__dirname, 'styles'), (err, data) => {
+//     data.forEach((style) => {
+//         if (path.extname(style) === '.css') {
+//             fs.readFile(path.join(__dirname, 'styles', style), (err, data) => {
+//                 if (err) return console.error(err);
+//                 fs.appendFile(
+//                     path.join(__dirname, 'project-dist', 'bundle.css'),
+//                     data,
+//                     (err) => {
+//                         if (err) return console.error(err);
+//                     }
+//                 );
+//             });
+//         }
+//     });
+// });
